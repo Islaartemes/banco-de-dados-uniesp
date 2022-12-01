1b0ce5ed00b25c83719d88715f244f40450fa750
Isla Artemes <islaartemes@gmail.com>
2022-12-01 17:03:56 -0300

    Atualizando projeto final

--- 4-Modelo físico/perguntas_projeto_final.sql
+++ 4-Modelo físico/perguntas_projeto_final.sql
@@ -1,4 +1,4 @@
-1. Quantos usuarios est√£o com assinatura ativa?
+-- 1. Quantos usuarios est„o com assinatura ativa?
 SELECT* FROM DBO.usuario;
 SELECT* FROM DBO.assinatura; 
 SELECT* FROM DBO.forma_pagamento; 
@@ -8,7 +8,7 @@ SELECT* FROM DBO.filme;
 SELECT* FROM DBO.serie; 
 SELECT* FROM DBO.unidades;
 
-2. Contar quantos registros tem em cada tabela;
+-- 2. Contar quantos registros tem em cada tabela;
 
 SELECT COUNT(*) FROM DBO.usuario;
 SELECT COUNT(*) FROM DBO.assinatura; 
@@ -19,37 +19,36 @@ SELECT COUNT(*) FROM DBO.filme;
 SELECT COUNT(*) FROM DBO.serie; 
 SELECT COUNT(*) FROM DBO.unidades;
 
-3. Informar quantas notas de avalia√ß√£o distintas existem para a tabela avalia√ß√£o na coluna nota_avaliacao;
+-- 3. Informar quantas notas de avaliaÁ„o distintas existem para a tabela avaliaÁ„o na coluna nota_avaliacao;
 
 SELECT COUNT (DISTINCT nota_avaliacao)
 FROM.DBO.avaliacao;
 
-
-4. Selecionar quais valores distintos existem para a tabela avalia√ß√£o na coluna nota_avaliacao;
+-- 4. Selecionar quais valores distintos existem para a tabela avaliaÁ„o na coluna nota_avaliacao;
 
 SELECT DISTINCT nota_avaliacao
 FROM avaliacao;
 
-5. Informar quantos valores distintos existem para a tabela m√≠dia na coluna tipo;
+-- 5. Informar quantos valores distintos existem para a tabela mÌdia na coluna tipo;
 
 SELECT COUNT (DISTINCT tipo)
 FROM midia;
 
-6. Selecionar quais valores distintos existem para a tabela m√≠dia na coluna tipo;
+-- 6. Selecionar quais valores distintos existem para a tabela mÌdia na coluna tipo;
 
 SELECT DISTINCT tipo
 FROM midia;
 
-7. Selecionar quais usu√°rios n√£o tem uma nota_avaliacao;
+-- 7. Selecionar quais usu·rios n„o tem uma nota_avaliacao;
 
 SELECT *FROM PET WHERE nota_avaliacao = NULL;
 
-9. Listar os usu√°rios ordenados pelo id;
+-- 9. Listar os usu·rios ordenados pelo id;
 
 SELECT F.NOME FROM DBO.usuario F
 ORDER BY F.id; 
 
-10. Listar os usu√°rios ordenados pelo nome;
+-- 10. Listar os usu·rios ordenados pelo nome;
 
 SELECT F.NOME FROM DBO.usuario F
 ORDER BY F.NOME ASC; 
