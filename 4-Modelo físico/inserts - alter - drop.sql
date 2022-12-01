36c98914dd49dc01dd89ade4fe07fa6228299034
Isla Artemes <islaartemes@gmail.com>
2022-12-01 16:30:44 -0300

    Alterações

--- 4-Modelo físico/inserts - alter - drop.sql
+++ 4-Modelo físico/inserts - alter - drop.sql
@@ -53,7 +53,85 @@ ALTER TABLE tb_assinatura ALTER COLUMN status_assinatura VARCHAR(25) NOT NULL
 
 UPDATE tb_assinatura SET status_assinatura = 'INATIVO' WHERE id_assinatura = 1;
 
+
+
+SELECT * FROM tb_filme
+
+--TABELA AVALIACAO
+
+INSERT INTO tb_avaliacao 
+(id_usuario, nota_avaliacao)
+VALUES
+(1, '10'),
+(2, '9'),
+(3, '5'),
+(4, '2'),
+(5, '2'),
+(6, '6'),
+(7, '10'),
+(8, '10'),
+(9, '8'),
+(10, '8'),
+(11, '10');
+
+INSERT INTO tb_midia
+(descricao_midia, nome_midia, tipo_midia, genero_midia)
+VALUES
+('Policial tenta salvar vítimas no natal', 'Duro de matar', 'F', 'Suspense'),
+('Relata o dia a dia de um advogado poderoso', 'Suits', 'S', 'Drama/Comédia'),
+('Viagens no tempo', 'Dark', 'S', 'Drama'),
+('Vampiro salva mocinha', 'Crepusculo', 'F', 'Drama'),
+('Bella fica com Jacob', 'Lua Nova', 'F', 'Drama'),
+('Bella entre Eduard e Jacob', 'Eclipse', 'F', 'Drama'),
+('Casamento de Bela e Eduard', 'Amanhecer parte um', 'F', 'Drama'),
+('Guerra com os Volture', 'Amanhecer parte dois', 'F', 'Drama'),
+('A vida de Lorelai e Rory', 'Gilmore Girls', 'S', 'Comédia'),
+('Uma menina muito legal', 'Wandinha', 'S', 'Suspense'),
+('Uma academia de polícia diferente', 'Brooklyn nine-nine', 'S', 'Comédia');
+
+INSERT INTO tb_comentario
+(conteudo_comentario, curtidas_comentario, id_midia, id_usuario)
+VALUES
+('Chato', '0', 1, 1),
+('Legal', '5', 2, 2),
+('Divertido', '8', 3, 3),
+('Genial', '10', 4, 4),
+('Ruim', '3', 5, 5),
+('Bom', '8', 6, 6),
+('Péssimo', '0', 7, 7),
+('Gostei', '9', 8, 8),
+('Legal', '10', 9, 9),
+('Legal', '9', 10, 10),
+('Aterrorizante', '9', 11, 11);
+
+INSERT INTO tb_serie
+(descricao_serie, id_midia)
+VALUES
+('Relata o dia a dia de um advogado poderoso', 2),
+('Viagens no tempo', 3),
+('A vida de Lorelai e Rory', 9),
+('Uma menina muito legal', 10),
+('Uma academia de polícia diferente', 11);
+
+INSERT INTO tb_filme
+(descricao_filme, id_midia)
+VALUES
+('Policial tenta salvar vítimas no natal', 1),
+('Vampiro salva mocinha', 4),
+('Bella fica com Jacob', 5),
+('Bella entre Eduard e Jacob', 6),
+('Casamento de Bela e Eduard', 7),
+('Guerra com os Volture', 8);
+
+INSERT INTO tb_unidades
+(temporada_unidades, episodio_unidades, nome_unidades, descricao_unidades, id_serie)
+VALUES
+('8', '10', 'Suits', 'Relata o dia a dia de um advogado poderoso', 1),
+('4', '8', 'Dark', 'Viagens no tempo', 2),
+('11', '22', ' Gilmore Girls', 'A vida de Lorelai e Rory', 3),
+('1', '10', 'Wandinha', 'Uma menina muito legal', 4),
+('7', '15', 'Brooklyn nine-nine', 'Uma academia de polícia diferente', 5);
+
+
 --DROPANDO TABELA
-
-DROP TABLE tb_avaliacao;
-
+DROP TABLE tb_segue;
