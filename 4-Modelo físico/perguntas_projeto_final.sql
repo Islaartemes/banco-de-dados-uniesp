6b62e98e2e2164bff45f1a803bce47e0fe576045
Isla Artemes <islaartemes@gmail.com>
2022-12-01 17:13:03 -0300

    Concluindo o projeto final

--- 4-Modelo físico/perguntas_projeto_final.sql
+++ 4-Modelo físico/perguntas_projeto_final.sql
@@ -1,55 +1,49 @@
--- 1. Quantos usuarios estão com assinatura ativa?
-SELECT* FROM DBO.usuario;
-SELECT* FROM DBO.assinatura; 
-SELECT* FROM DBO.forma_pagamento; 
-SELECT* FROM DBO.avaliacao;
-SELECT* FROM DBO.comentario;
-SELECT* FROM DBO.filme; 
-SELECT* FROM DBO.serie; 
-SELECT* FROM DBO.unidades;
+-- 1. Selecione todos os usuários da tabela de usuários?
+SELECT * FROM tb_usuario;
+
 
 -- 2. Contar quantos registros tem em cada tabela;
 
-SELECT COUNT(*) FROM DBO.usuario;
-SELECT COUNT(*) FROM DBO.assinatura; 
-SELECT COUNT(*) FROM DBO.forma_pagamento; 
-SELECT COUNT(*) FROM DBO.avaliacao;
-SELECT COUNT(*) FROM DBO.comentario;
-SELECT COUNT(*) FROM DBO.filme; 
-SELECT COUNT(*) FROM DBO.serie; 
-SELECT COUNT(*) FROM DBO.unidades;
+SELECT COUNT(*) FROM tb_usuario;
+SELECT COUNT(*) FROM tb_assinatura; 
+SELECT COUNT(*) FROM tb_forma_pagamento; 
+SELECT COUNT(*) FROM tb_avaliacao;
+SELECT COUNT(*) FROM tb_comentario;
+SELECT COUNT(*) FROM tb_filme; 
+SELECT COUNT(*) FROM tb_serie; 
+SELECT COUNT(*) FROM tb_unidades;
 
 -- 3. Informar quantas notas de avaliação distintas existem para a tabela avaliação na coluna nota_avaliacao;
 
 SELECT COUNT (DISTINCT nota_avaliacao)
-FROM.DBO.avaliacao;
+FROM tb_avaliacao;
 
 -- 4. Selecionar quais valores distintos existem para a tabela avaliação na coluna nota_avaliacao;
 
 SELECT DISTINCT nota_avaliacao
-FROM avaliacao;
+FROM tb_avaliacao;
 
 -- 5. Informar quantos valores distintos existem para a tabela mídia na coluna tipo;
 
-SELECT COUNT (DISTINCT tipo)
-FROM midia;
+SELECT COUNT (DISTINCT tipo_midia)
+FROM tb_midia;
 
 -- 6. Selecionar quais valores distintos existem para a tabela mídia na coluna tipo;
 
-SELECT DISTINCT tipo
-FROM midia;
+SELECT DISTINCT tipo_midia
+FROM tb_midia;
 
 -- 7. Selecionar quais usuários não tem uma nota_avaliacao;
 
-SELECT *FROM PET WHERE nota_avaliacao = NULL;
+SELECT *FROM tb_avaliacao WHERE nota_avaliacao = NULL;
 
 -- 9. Listar os usuários ordenados pelo id;
 
-SELECT F.NOME FROM DBO.usuario F
-ORDER BY F.id; 
+SELECT F.nome_usuario FROM tb_usuario F
+ORDER BY F.id_usuario; 
 
 -- 10. Listar os usuários ordenados pelo nome;
 
-SELECT F.NOME FROM DBO.usuario F
-ORDER BY F.NOME ASC; 
+SELECT F.nome_usuario FROM tb_usuario F
+ORDER BY F.nome_usuario ASC; 
 
